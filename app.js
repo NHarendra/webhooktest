2e4448db6f868728b2e0263669784fa58f9c56bb
NHarendra <harendra@innolat.com>
2021-10-30 16:01:49 +0530

    body log

--- app.js
+++ app.js
@@ -85,6 +85,7 @@ app.get("/webhook", (req, res) => {
 app.post("/webhook", (req, res) => {
   let body = req.body;
   console.log('Webhook Called By Harendra');
+  console.log(body);
   console.log(`\u{1F7EA} Received webhook:`);
   console.dir(body, { depth: null });
 
